ebcc24df638709ea62721bebbfc278c748d83f64
shaokp <286096598@qq.com>
2020-10-30 22:13:17 +0800

    Site updated: 2020-10-30 22:13:14

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c47be8dce38f4bbacc49180cf749e7b3";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
